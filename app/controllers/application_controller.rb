6f009abaaeab3937bc85732303d0063b4a8da7cc
Alysson A Costa <alysson.avila.costa@gmail.com>
2017-10-22 23:53:23 +0000

    Adicionados novos campos para registro do usuário. Incluir validações no model User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :set_referer, only: [:new, :edit]
+  before_action :configure_devise_params, if: :devise_controller?
   protect_from_forgery with: :exception
 
   private
   def set_referer
   	session[:referer] = request.referer
+  end
+
+  def configure_devise_params
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :alias, :email, :college, :password, :password_confirmation) }
   end
 end
