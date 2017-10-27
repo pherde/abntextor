59b719792c7d98d630e0cbee62d1b7f716f3b0b5
Alysson A Costa <alysson.avila.costa@gmail.com>
2017-10-27 00:04:48 +0000

    Validações no cadastro do usuário implementadas. Algumas mensagens estão configuradas no arquivo de localização, pois as constraints foram geradas na migração do Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  # Validações: as constraints definidas na migration do Devise tem suas mensagens configuradas no arquido de localização
+  # Ver: config/locales/pt.yml
+  validates :name, presence: { message: "O campo Nome completo é obrigatório" }
+  validates :alias, presence: { message: "O campo Nome de usuário (Apelido) é obrigatório" }
+  validates :alias, uniqueness: { message: "Esse apelido já foi cadastrado. Por gentileza, escolha outro" }
+  validates :email, presence: { message: "O campo E-mail é obrigatório" }
+  validates :email, uniqueness: {message: "Esse e-mail ja foi cadastrado. Recupere sua senha clicando <a href='#'>aqui<a/>".html_safe }
+  validates :password, presence: { message: "O campo Senha é obrigatório" }
+  validates :password_confirmation, presence: { message: "O campo de confirmação de senha é obrigatório" }
+
   def admin?
   	self.is_admin
   end
