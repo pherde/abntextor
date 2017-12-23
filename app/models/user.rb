d38eed272f47e10f323c51c445af25a64de07bff
Alysson A COSTA <alysson.avila.costa@gmail.com>
2017-12-23 21:09:14 +0000

    Melhora da disposição do Gravatar. Exclusão de usuário exclui trabalhos em cascata

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :papers
+  has_many :papers, dependent: :destroy
 
   #Gravatar
   include Gravtastic
@@ -10,15 +10,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # Validações: as constraints definidas na migration do Devise tem suas mensagens configuradas no arquido de localização
-  # Ver: config/locales/pt.yml
   validates :name, presence: { message: "O campo Nome completo é obrigatório" }
   validates :alias, presence: { message: "O campo Nome de usuário (Apelido) é obrigatório" }
   validates :alias, uniqueness: { message: "Esse apelido já foi cadastrado. Por gentileza, escolha outro" }
   validates :email, presence: { message: "O campo E-mail é obrigatório" }
   validates :email, uniqueness: {message: "Esse e-mail ja foi cadastrado. Recupere sua senha clicando <a href='#'>aqui<a/>".html_safe }
-  #validates :password, presence: { message: "O campo Senha é obrigatório" }
-  #validates :password_confirmation, presence: { message: "O campo de confirmação de senha é obrigatório" }
 
   def admin?
   	self.is_admin
